35e76fa61a4dd2bbe3c3d119ce43d4e2b6705987
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-06-06 16:13:06 -0400

    Improve code documentation

--- derrida/books/models.py
+++ derrida/books/models.py
@@ -55,11 +55,13 @@ class InstanceCount(models.Model):
 
 class Subject(Named, Notable, WorkCount):
     '''Subject categorization for books'''
+    #: optional uri
     uri = models.URLField(blank=True, null=True)
 
 
 class Language(Named, Notable, WorkCount, InstanceCount):
     '''Language that a book is written in or a language included in a book'''
+    #: optional uri
     uri = models.URLField(blank=True, null=True)
 
 
@@ -70,9 +72,12 @@ class Publisher(Named, Notable, InstanceCount):
 
 class OwningInstitution(Named, Notable, InstanceCount):
     '''Institution that owns the extant copy of a book'''
+    #: short name (optioal)
     short_name = models.CharField(max_length=255, blank=True,
         help_text='Optional short name for admin display')
+    #: contact information
     contact_info = models.TextField()
+    #: :class:`~derrida.places.models.Place`
     place = models.ForeignKey(Place)
 
     def __str__(self):
@@ -90,17 +95,25 @@ class Journal(Named, Notable):
 
 class Work(Notable):
     '''A platonic work.  Stores common information about multiple
-    instances, copies, or editions of the same work.'''
+    instances, copies, or editions of the same work.  Aggregates one
+    or more :class:`Instance` objects.'''
+    #: primary title
     primary_title = models.TextField()
+    #: short title
     short_title = models.CharField(max_length=255)
+    #: original publication date
     year = models.IntegerField(blank=True, null=True,
         help_text='Original publication date')
     # NOTE: this is inteneded for a generic linked data URI;
     # finding aid URL should be tracked on Instance rather than Work
+    #: optional URI
     uri = models.URLField('URI', blank=True, help_text='Linked data URI',
         default='')
+    #: relation to :class:`Person` authors
     authors = models.ManyToManyField(Person)
+    #: :class:`Subject` related through :class:`WorkSubject`
     subjects = models.ManyToManyField(Subject, through='WorkSubject')
+    #: :class:`Language` related through :class:`WorkLanguage`
     languages = models.ManyToManyField(Language, through='WorkLanguage')
 
     class Meta:
@@ -111,7 +124,7 @@ class Work(Notable):
         return '%s (%s)' % (self.short_title, self.year or 'n.d.')
 
     def author_names(self):
-        'Display author names; convenience access for display in admin'
+        '''Display author names; convenience access for display in admin'''
         # NOTE: possibly might want to use last names here
         return ', '.join(str(auth) for auth in self.authors.all())
     author_names.short_description = 'Authors'
@@ -129,14 +142,20 @@ class Work(Notable):
 
 
 class Instance(Notable):
-    '''A single instance of a work - i.e., a specific copy or edition
+    '''A single instance of a :class:`Work` - i.e., a specific copy or edition
     or translation.  Can also include books that appear as sections
     of a collected works.'''
+
+    #: :class:`Work` this instancebelongs to
     work = models.ForeignKey(Work)
+    #: alternate title (optional)
     alternate_title = models.CharField(blank=True, max_length=255)
+    #: :class:`Publisher` (optional)
     publisher = models.ForeignKey(Publisher, blank=True, null=True)
+    #: publication :class:`~derrida.places.models.Place` (optional, sorted many to many)
     pub_place = SortedManyToManyField(Place,
         verbose_name='Place(s) of Publication', blank=True)
+    #: Zotero identifier
     zotero_id = models.CharField(
         max_length=255,
         # Add validator for any Zotero IDs entered manually by form.
@@ -146,46 +165,68 @@ class Instance(Notable):
                         message='Zotero IDs must be alphanumeric.'
                     )]
     )
+    #: item is extant
     is_extant = models.BooleanField(help_text='Extant in PUL JD', default=False)
+    #: item is annotated
     is_annotated = models.BooleanField(default=False)
+    #: item is translated
     is_translation = models.BooleanField(default=False)
+    #: description of item dimensions (optional)
     dimensions = models.CharField(max_length=255, blank=True)
+    #: copyright year
     copyright_year = models.PositiveIntegerField(blank=True, null=True)
+    #: related :class:`Journal` for a journal article
     journal = models.ForeignKey(Journal, blank=True, null=True)
     print_date_help_text = 'Date as YYYY-MM-DD, YYYY-MM, or YYYY format. Use' \
         + ' print date day/month/year known flags to indicate' \
         + ' that the information is not known.'
+    #: print date
     print_date = models.DateField('Print Date',
         blank=True, null=True, help_text=print_date_help_text)
+    #: print date day is known
     print_date_day_known = models.BooleanField(default=False)
+    #: print date month is known
     print_date_month_known = models.BooleanField(default=False)
+    #: print date year is known
     print_date_year_known = models.BooleanField(default=True)
+    #: finding aid URL
     uri = models.URLField('URI', blank=True, default='',
         help_text='Finding Aid URL for items in PUL Derrida Library')
+    # item has a dedication
     has_dedication = models.BooleanField(default=False)
+    # item has insertiosn
     has_insertions = models.BooleanField(default=False)
     # page range: using character fields to support non-numeric pages, e.g.
     # roman numerals for introductory pages; using two fields to support
     # sorting within a volume of collected works.
+    #: start page for book section or journal article
     start_page = models.CharField(max_length=20, blank=True, null=True)
+    #: end page for book section or journal article
     end_page = models.CharField(max_length=20, blank=True, null=True)
 
+    #: :class:`Language` this item is written in;
+    # uses :class:`InstanceLanguage` to indicate primary language
     languages = models.ManyToManyField(Language, through='InstanceLanguage')
 
+    #: :class:`Instance` that collects this item, for book section
     collected_in = models.ForeignKey('self', related_name='collected_set',
         on_delete=models.SET_NULL, blank=True, null=True,
         help_text='Larger work instance that collects or includes this item')
     # work instances are connected to owning institutions via the Catalogue
     # model; mapping as a many-to-many with a through
     # model in case we want to access owning instutions directly
+
+    #: :class:`OwningInstitution`; connected through :class:`InstanceCatalogue`
     owning_institutions = models.ManyToManyField(OwningInstitution,
         through='InstanceCatalogue')
 
+    #: :class:`DerridaWork` this item is cited in
     cited_in = models.ManyToManyField('DerridaWork',
         help_text='Derrida works that cite this edition or instance',
         blank=True)
 
     # proof-of-concept generic relation to footnotes
+    #: generic relation to :class:~`derrida.footnotes.models.Footnote`
     footnotes = GenericRelation(Footnote)
 
     class Meta:
@@ -202,11 +243,13 @@ class Instance(Notable):
             self.copyright_year or 'n.d.')
 
     def display_title(self):
+        '''display title - alternate title or work short title'''
         return self.alternate_title or self.work.short_title or '[no title]'
     display_title.short_description = 'Title'
 
     @property
     def item_type(self):
+        '''item type: book, book section, or journal article'''
         if self.journal:
             return 'Journal Article'
         if self.collected_in:
@@ -214,13 +257,13 @@ class Instance(Notable):
         return 'Book'
 
     def author_names(self):
-        'Display Work author names; convenience access for display in admin'
+        '''Display Work author names; convenience access for display in admin'''
         return self.work.author_names()
     author_names.short_description = 'Authors'
     author_names.admin_order_field = 'work__authors__authorized_name'
 
     def catalogue_call_numbers(self):
-        'Convenience access to catalogue call numbers, for display in admin'
+        '''Convenience access to catalogue call numbers, for display in admin'''
         return ', '.join([c.call_number for c in self.instancecatalogue_set.all()
                           if c.call_number])
     catalogue_call_numbers.short_description = 'Call Numbers'
@@ -230,8 +273,11 @@ class Instance(Notable):
 class WorkSubject(Notable):
     '''Through-model for work-subject relationship, to allow designating
     a particular subject as primary or adding notes.'''
+    #: :class:`Subject`
     subject = models.ForeignKey(Subject)
+    #: :class:`Work`
     work = models.ForeignKey(Work)
+    #: boolean flag indicating if this subject is primary for this work
     is_primary = models.BooleanField(default=False)
 
     class Meta:
@@ -246,8 +292,11 @@ class WorkSubject(Notable):
 class WorkLanguage(Notable):
     '''Through-model for work-language relationship, to allow designating
     one language as primary or adding notes.'''
+    #: :class:`Language`
     language = models.ForeignKey(Language)
+    #: :class:`Work`
     work = models.ForeignKey(Work)
+    #: boolean flag indicating if this language is primary for this work
     is_primary = models.BooleanField()
 
     class Meta:
@@ -262,8 +311,11 @@ class WorkLanguage(Notable):
 class InstanceLanguage(Notable):
     '''Through-model for instance-language relationship, to allow designating
     one language as primary or adding notes.'''
+    #: :class:`Language`
     language = models.ForeignKey(Language)
+    #: :class:`Instance`
     instance = models.ForeignKey(Instance)
+    #: boolean flag indicating if this language is primary for this instance
     is_primary = models.BooleanField()
 
     class Meta:
@@ -580,8 +632,11 @@ class PersonBook(Notable, DateRange):
 class DerridaWork(Notable):
     '''This models the reference copy used to identify all citations, not
     part of Derrida's library'''
+    #: short title
     short_title = models.CharField(max_length=255)
+    #: full citation
     full_citation = models.TextField()
+    #: boolean indicator for primary work
     is_primary = models.BooleanField()
     cited_books = models.ManyToManyField(
         'Book',
@@ -620,15 +675,23 @@ class ReferenceType(Named, Notable):
 
 
 class Reference(models.Model):
-    '''References to Derrida's works from Zotero Tags collected by team'''
+    '''Reference to a book from a work by Derrida.  Can be a citation,
+    quotation, or other kind of reference.'''
     book = models.ForeignKey(Book)
     # reference both book and instance until book data is migrated
+    #: :class:`Instance` that is referenced
     instance = models.ForeignKey(Instance, blank=True, null=True)
+    #: :class:`DerridaWork` that references the item
     derridawork = models.ForeignKey(DerridaWork)
+    #: page in the Derrida work
     derridawork_page = models.CharField(max_length=10)
+    #: location/identifier on the page
     derridawork_pageloc = models.CharField(max_length=2)
+    #: page in the referenced item
     book_page = models.CharField(max_length=255, blank=True)
+    #: :class:`ReferenceType`
     reference_type = models.ForeignKey(ReferenceType)
+    #: anchor text
     anchor_text = models.TextField(blank=True)
 
     class Meta:
