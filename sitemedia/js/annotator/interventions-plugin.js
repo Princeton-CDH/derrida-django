b057ad999a538dd59e6452d0a3f2452fbc7daa59
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-06-20 17:28:26 -0400

    Adapt winthrop annotator plugin for tag autocomplete

--- sitemedia/js/annotator/interventions-plugin.js
+++ sitemedia/js/annotator/interventions-plugin.js
@@ -1,8 +1,55 @@
 
 var interventions = {
 
-    /* annotation viewer method, for customizing marginalia display;
-   only handles annotator text */
+    /**
+     * Parse and return the last value from a comma-delimited string
+     * @param {string}
+     */
+    last_term: function(str) {
+      var terms = str.split(',');
+      return terms[terms.length - 1].trim();
+    },
+
+    /**
+     * Convert django-autocomplete-light response into the format
+     * jquery-ui expects.
+     * @param data
+     */
+    data_for_jqui: function(data) {
+      return $.map(data.results, function(value, key) {
+          return {
+              label: value.text,
+              value: value.text,
+              id: value.id
+          };
+        });
+    },
+
+    /**
+     * Update a comma-separated list text input with the selected value
+     * @param text - input element
+     * @param value - newly selected value
+     */
+    multival_select: function(input, value) {
+      // store values as a list on the input data
+      if (input.data('value') == undefined) {
+        // initialize as empty list if not set
+        input.data('value', []);
+      }
+      // retrieve the current list of values and add the new item
+      var val_list = input.data('value');
+      val_list.push(value);
+      // display comma separated list and store the updated list
+      input.val(val_list.join(', ') + ', ');
+      input.data('value', val_list)
+    },
+
+
+    /**
+     *  Annotation viewer method, for customizing marginalia display.
+     * Only handles annotation text field display.
+     * @param annotation
+     */
    viewer: function(annotation) {
       // default behavior
       if (annotation.text) {
@@ -14,17 +61,115 @@ var interventions = {
       }
     },
 
-    editorExtension: function(editor) {
-        // update main annotation text field so label and placeholders
-        // make sense for how it is being used in this project
-        editor.fields[0].label = 'Annotation Text';
-        // editor inputs have already been generated at this point
-        var text_input = editor.element.find('textarea').first();
-        // replace default placeholder text ('Comments...')
-        text_input.attr('placeholder', 'Transcription of annotation text, if any');
-        // add a label
-        text_input.before('<label class="field-label">Text</label>');
+    /**
+     * Generate and return an annotator editor extension based on the
+     * specified set of fields.
+     * @param confs
+     */
+    getEditorExtension: function(confs) {
 
+      return function editorExtension(editor) {
+          // update built-in annotation text field so label and placeholders
+          // make sense for how it is being used in this project
+          // TODO: could these overrides be included in the config?
+          editor.fields[0].label = 'Annotation Text';
+          // editor inputs have already been generated at this point
+          var text_input = editor.element.find('textarea').first();
+          // replace default placeholder text ('Comments...')
+          text_input.attr('placeholder', 'Transcription of annotation text, if any');
+          // add a label
+          text_input.before('<label class="field-label">Text</label>');
+
+          // for each item in the config, add a new annotator field
+          // and configure appropriate load/save methods
+          $.each(confs, function(index, config) {
+            var type = config.size ? config.size : 'input';
+            var field = editor.addField({
+              // set input name based on field name
+              id: 'annotator-' + config.name,
+              // This is properly *placeholder*, so setting is as such
+              // using label as fallback
+              label: config.placeholder ? config.placeholder : config.label,
+              type: type,
+              // load the field for display when the editor is rendered
+              load: function(field, annotation) {
+                  // determine value to set in the input for this field;
+                  // must be set to something to avoid carrying over
+                  // values from other annotation instances
+                  var display_val = '',
+                    $input = $(field).find(type);
+
+                  // retrieve field value from the annotation object
+                  if (annotation[config.name] || annotation[config.name == 0]) {
+                      // store the value on input element data
+                      $input.data('value', annotation[config.name]);
+                      // populate input value the annotation data
+                      display_val = annotation[config.name];
+                      // convert to comma-delimited if list is configured
+                      if (config.list) {
+                        display_val = display_val.join(', ') + ', ';
+                      }
+                  }
+                  // set the input value
+                  $input.val(display_val);
+              },
+              submit: function(field, annotation) {
+                var $input = $(field).find(type);
+                // send data version without worrying about display value
+                annotation[config.name] = $input.data('value');
+              },
+              // store element config on the editor field in case we need it later
+              config: config
+            });
+
+            // initialize autocomplete field if a url is configured
+            if (config.autocompleteURL) {
+              // find the input element for this field and configure
+              // the autocomplete widget
+              var input = $(field).find(type);
+              input.autocomplete({
+                    minLength: 0,
+                    // Retrieve values via ajax
+                    source: function(request, response) {
+                        term = interventions.last_term(request.term);
+                        // autoCompleteUrl from conf function
+                        $.get(config.autocompleteURL, {q: term},
+                            function(data) {
+                              response(interventions.data_for_jqui(data))
+                            }
+                        );
+                    },
+                    focus: function(event, ui) {
+                      event.preventDefault();
+                    },
+                    open: function(event, ui) {
+                      console.log('open');
+                      // annotator purposely sets the editor at a very high z-index;
+                      // set autocomplete still higher so it isn't obscured by annotator buttons
+                      $('.ui-autocomplete')
+                        .css('z-index', $('.annotator-editor').css('z-index') + 1);
+                    },
+                });
+
+                // multi-item autocompletes need special handling to
+                // add selected item to the list of existing values
+                if (config.list) {
+                  input.autocomplete({
+                    select: function(event, ui) {
+                      interventions.multival_select($(this), ui.item.value);
+                      event.preventDefault();
+                    }
+                  });
+                }
+
+                // Trigger autocomplete start on focus
+                input.bind('focus', function() {
+                  $(this).autocomplete("search");
+                });
+
+            }
+          });
+
+      }
     }
-
 };
