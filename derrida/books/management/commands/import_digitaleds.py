ff308959031212511ae7b5d2447e32612b5aeea8
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-07-13 11:29:41 -0400

    Update manifest import to match instances on findingaid url #7
    Also adds brief summary output

--- derrida/books/management/commands/import_digitaleds.py
+++ derrida/books/management/commands/import_digitaleds.py
@@ -18,6 +18,7 @@ When a local identifier is present in manifest metadata, it will be used
 to link the cached manifest in the django database with the appropriate
 :class:`winthrop.books.models.Book`.
 '''
+from collections import defaultdict
 
 from django.core.management.base import BaseCommand
 from djiffy.importer import ManifestImporter
@@ -30,41 +31,58 @@ class DerridaManifestImporter(ManifestImporter):
     logic for associating the imported :class:`djiffy.models.Manifest`
     with an existing :class:`winthrop.books.models.Book`'''
 
+    stats = defaultdict(int)
+
     def import_manifest(self, manifest, path):
         # parent method returns newly created db manifest
         # or None if there was an error or manifest was already imported
+        self.stats['urls'] += 1
         db_manifest = super(DerridaManifestImporter, self) \
             .import_manifest(manifest, path)
         if not db_manifest:
             return
 
-        # **NOTE** source metadata identifier implementation is provisional,
-        # since that field is not yet included in the manifest seeAlso data
+        short_id = db_manifest.short_id
+        self.stats['manifests'] += 1
+
+        self.output('Imported %s "%s"' % (short_id, db_manifest.label))
 
         # Attempt to find the corresponding Derrida library instance object
         # for this digital edition and associate them.
-        # - manifests from plum include a source metadata identifier
-        #   that looks like RBD1.1_c478
-        #   Use that to find instances in the local database by
-        #   finding aid url, which looks like:
-        #   http://findingaids.princeton.edu/collections/RBD1/c9157
-        if 'Source metadata identifier' in db_manifest.metadata:
-            source_id = db_manifest.metadata['Source metadata identifier'][0]
-            fa_collection, item_id = source_id.split('.')
-            findingaid_url = 'http://findingaids.princeton.edu/collections/%s/%s' % \
-                (fa_collection, item_id)
-            items = Instance.objects.filter(uri=findingaid_url)
+        # - manifests from plum include a url identifier as a "seeAlso"
+        # link; for archival items, this is the finding aid url.
+        findingaid_url = None
+        for url in db_manifest.extra_data.keys():
+            if url.startswith('https://findingaids.princeton.edu'):
+                findingaid_url = url
+                break
+
+        # if a finding aid url was found, clean it up for matching
+        # against the finding aid urls in project data
+        if findingaid_url:
+            # remove https since local urls use http
+            findingaid_url = findingaid_url.replace('https://', '')
+            # remove trailing .xml?scope=record for matching purposes
+            findingaid_url = findingaid_url.split('.xml')[0]
+
+            items = Instance.objects.filter(uri__contains=findingaid_url)
             # only associate if one and only one match is found
             if items.count() == 1:
                 instance = items.first()
                 instance.digital_edition = db_manifest
                 instance.save()
-
+                self.output('. Associated %s with "%s"' % (short_id, instance))
+            elif items.count() > 1:
+                self.error_msg('. Found %d matching instances for %s' % \
+                    (items.count(), findingaid_url))
+                self.stats['nomatch'] += 1
             else:
-                self.error_msg('No matching instance for %s' % source_id)
+                self.error_msg('. No matching instance for %s (%s)' % \
+                    (short_id, findingaid_url))
+                self.stats['nomatch'] += 1
 
         else:
-            self.error_msg('No source metadata identifier found')
+            self.error_msg('No finding aid URL found for %s' % short_id)
 
         return db_manifest
 
@@ -87,8 +105,16 @@ class Command(BaseCommand):
         # convert any shorthand ids into the appropriate manifest uri
         manifest_paths = [self.manifest_uris[p] if p in self.manifest_uris else p
                           for p in kwargs['path']]
-        DerridaManifestImporter(stdout=self.stdout, stderr=self.stderr,
-                                 style=self.style) \
-            .import_paths(manifest_paths)
+        dmi = DerridaManifestImporter(stdout=self.stdout, stderr=self.stderr,
+                                     style=self.style)
+        dmi.import_paths(manifest_paths)
+
+        # briefly summarize what was done
+        print('\nURLs processed: %(urls)d' % dmi.stats)
+        if dmi.stats['manifests']:
+            print('Manifests imported: %(manifests)d' % dmi.stats)
+            if dmi.stats['nomatch']:
+                print('Manifests not matched to library work instances: %(nomatch)d' \
+                    % dmi.stats)
 
 
