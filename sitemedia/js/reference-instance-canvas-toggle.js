3932b80bc9d6bc4c909863630d742acc3474b82d
bwhicks <benjamin.w.hicks@gmail.com>
2017-07-20 11:50:50 -0400

    Revise handling for #14, #16 post QA deploy

--- sitemedia/js/reference-instance-canvas-toggle.js
+++ sitemedia/js/reference-instance-canvas-toggle.js
@@ -19,13 +19,29 @@ function toggleReferenceAutocompletes() {
       $('.get_autocomplete_instances div div .grp-readonly').text()
     );
   }
+
+  // Flag to see if a digital edition is set on Instance change_form
+  var digitalEdFlag = $('#id_digital_edition option:selected').val();
+
+  // Flag to see if an instance is selected on Reference change_form
+  var selectedInstancePk = $('#id_instance').val()
+  // Flag to see if instance on Reference change_form is in array of
+  // Instances with digital editions
+  var pkInArray = instancePksArray.indexOf(parseInt(selectedInstancePk));
+
   /*
-  Check if there is an instance selected for Reference change_form or a
-  digital edition chosen on the Instance change_form, and if not, disable
-  the autcompletes.
+  Check if:
+  1) There is an instance selected for Reference change_form and if it is
+  on the list of those with digital editions.
+  2) There is  a digital edition set on the Instance change_form.
+
+  If either are not true, disable the selectors
   */
-  if ($('#id_instance option:selected').val() == "" ||
-    $('#id_digital_edition option:selected').val() == "") {
+      // value of #id_instance is not in the list of digital editions or
+      // #id_instance doesn't exist
+  if ((pkInArray == -1 && $('#id_instance').val() != undefined) ||
+      // #digital_edition select is either not set or doesn't exist
+      (digitalEdFlag == "" && digitalEdFlag != undefined)) {
     $('select[id*="canvases"]').attr('disabled', true);
     $('select[id*="interventions"]').attr('disabled', true);
 
@@ -37,8 +53,8 @@ function toggleReferenceAutocompletes() {
     For the change form, use the parsed JSON from get_autocomplete_instances
     read-only field.
     */
-    var digitalEdFlag = $('#id_digital_edition option:selected').val();
-    var pkInArray = instancePksArray.indexOf($('#id_instance').val());
+    // if #id_instance exists and is in the array or check or
+    // if #digital_edition exists and is set
     if (pkInArray == 0 || (digitalEdFlag != "" && digitalEdFlag != undefined)) {
       $('select[id*="canvases"]').removeAttr('disabled', true);
       $('select[id*="interventions"]').removeAttr('disabled', true);
@@ -53,4 +69,6 @@ $(document).ready(function() {
   $("#id_instance").change(function() {
     toggleReferenceAutocompletes();
   });
+  // Do NOT bind on page load for Instance change_form, since autocompletes need
+  // a saved instance to be accruate. 
 });
