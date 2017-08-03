8188008cf1bd41c1cbe0452ff635dd0319dfecd9
bwhicks <benjamin.w.hicks@gmail.com>
2017-08-03 11:06:39 -0400

    Add trailing slash to url

--- derrida/books/urls.py
+++ derrida/books/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
         name='publisher-autocomplete'),
     url(r'^languages/autocomplete/$', staff_member_required(LanguageAutocomplete.as_view()),
         name='language-autocomplete'),
-    url(r'^(?P<pk>\d+)$', InstanceDetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>\d+)/$', InstanceDetailView.as_view(), name='detail'),
     url(r'^$', InstanceListView.as_view(), name='list'),
 ]
