9d6af7a65c49ad18ecce663342c3f15a2fcf897a
Kevin Glover <hkglover@gmail.com>
2017-08-03 20:46:13 -0400

    Add spaces to organize urls

--- derrida/urls.py
+++ derrida/urls.py
@@ -14,9 +14,11 @@ urlpatterns = [
     # for now, since there is not yet any public-facing site,
     # redirect base url to admin index page
     url(r'^$', RedirectView.as_view(pattern_name='admin:index'), name='site-index'),
+
     # placeholders for new design
     url(r'^citations/$', RedirectView.as_view(pattern_name='admin:index'), name='citations-list'),
-    # # grappelli URLS for admin related lookups & autocompletes
+
+    # grappelli URLS for admin related lookups & autocompletes
     url(r'^grappelli/', include('grappelli.urls')),
 
     url(r'^admin/', admin.site.urls),
@@ -25,10 +27,13 @@ urlpatterns = [
     url(r'^people/', include('derrida.people.urls', namespace='people')),
     url(r'^places/', include('derrida.places.urls', namespace='places')),
     url(r'^interventions/', include('derrida.interventions.urls', namespace='interventions')),
+
     # local version of djiffy urls
     url(r'^admin/iiif-books/', include('derrida.interventions.iiif_urls', namespace='djiffy')),
-     # annotations API
+
+    # annotations API
     url(r'^annotations/api/', include('annotator_store.urls', namespace='annotation-api')),
+
     # annotatorjs doesn't handle trailing slash in api prefix url
     url(r'^annotations/api', annotator_views.AnnotationIndex.as_view(), name='annotation-api-prefix'),
 
