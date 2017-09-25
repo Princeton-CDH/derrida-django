14f0aca71c19c4448df76b6daf76853d31a4945c
Kevin Glover <hkglover@gmail.com>
2017-09-25 12:42:53 -0400

    Check for jQuery before init ribbon

--- sitemedia/js/local.js
+++ sitemedia/js/local.js
@@ -1,13 +1,15 @@
-$(document).ready(function(){
-    var $ribbon = $('.ribbon');
-    if ($ribbon) {
-        var faded = sessionStorage.getItem('fade-test-banner', true);
-        if (! faded) {
-            $('.ribbon-box').removeClass('fade');
-        }
-        $ribbon.on('click',function(){
-            $('.ribbon-box').addClass('fade');
-            sessionStorage.setItem('fade-test-banner', true);
-        });
-    }
-});
+if ('undefined' !== typeof window.jQuery || 'undefined' !== typeof window.$) {
+  $(function() {
+      var $ribbon = $('.ribbon');
+      if ($ribbon) {
+          var faded = sessionStorage.getItem('fade-test-banner', true);
+          if (! faded) {
+              $('.ribbon-box').removeClass('fade');
+          }
+          $ribbon.on('click',function(){
+              $('.ribbon-box').addClass('fade');
+              sessionStorage.setItem('fade-test-banner', true);
+          });
+      }
+  });
+}
