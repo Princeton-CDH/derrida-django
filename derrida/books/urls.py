7d31ed039707f557565431b47b4d70b700f37489
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-09-29 15:06:47 -0400

    Group book urls so they are easier to read & work with

--- derrida/books/urls.py
+++ derrida/books/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib.admin.views.decorators import staff_member_required
 
 from derrida.books import views
@@ -14,33 +14,39 @@ urlpatterns = [
     # cross-item search
     url(r'^search/$', views.SearchView.as_view(), name='search'),
 
-    url(r'^references/$', views.ReferenceListView.as_view(), name='reference-list'),
-    url(r'^references/histogram/$', views.ReferenceHistogramView.as_view(),
-        name='reference-histogram'),
-    url(r'^references/histogram/(?P<derridawork_slug>[a-z-]+)/$',
-        views.ReferenceHistogramView.as_view(), {'mode': 'section'},
-        name='reference-histogram'),
-    url(r'^references/(?P<derridawork_slug>[a-z-]+)/(?P<page>[\dA-Z]+)(?P<pageloc>[a-z]+)?/$',
-        views.ReferenceDetailView.as_view(), name='reference'),
+    # reference views
+    url(r'^references/', include([
+        url(r'^$', views.ReferenceListView.as_view(), name='reference-list'),
+        url(r'^histogram/$', views.ReferenceHistogramView.as_view(),
+            name='reference-histogram'),
+        url(r'^histogram/(?P<derridawork_slug>[a-z-]+)/$',
+            views.ReferenceHistogramView.as_view(), {'mode': 'section'},
+            name='reference-histogram'),
+        url(r'^(?P<derridawork_slug>[a-z-]+)/(?P<page>[\dA-Z]+)(?P<pageloc>[a-z]+)?/$',
+            views.ReferenceDetailView.as_view(), name='reference'),
+    ])),
 
-    url(r'^library/$', views.InstanceListView.as_view(), name='list'),
-    url(r'^library/(?P<slug>[-\w]+)/$', views.InstanceDetailView.as_view(), name='detail'),
-    url(r'^library/(?P<slug>[-\w]+)/gallery/$',
-        views.InstanceDetailView.as_view(template_name='books/detail/gallery.html'),
-        name='detail-gallery'),
-    url(r'^library/(?P<slug>[-\w]+)/citations/$',
-        views.InstanceReferenceDetailView.as_view(), name='detail-citations'),
+    # library views
+    url(r'^library/', include([
+        url(r'^$', views.InstanceListView.as_view(), name='list'),
+        url(r'^(?P<slug>[-\w]+)/', include([
+            url(r'^$', views.InstanceDetailView.as_view(), name='detail'),
+            url(r'^citations/$',
+                views.InstanceReferenceDetailView.as_view(), name='detail-citations'),
+            url(r'^gallery/$',
+                views.InstanceDetailView.as_view(template_name='books/detail/gallery.html'),
+                name='detail-gallery'),
+            url(r'^gallery/(?P<short_id>[a-z0-9]+)/$',
+                views.CanvasDetail.as_view(), name='canvas-detail'),
+            # canvas image views
+            url(r'^gallery/images/(?P<page_num>[0-9]{1,4})-?[0-9]*[a-z]?/$',
+                views.CanvasImageByPageNumber.as_view(), {'mode': 'by-page'}, name='canvas-by-page'),
+            url(r'^gallery/images/(?P<short_id>[a-z0-9]+)/(?P<mode>thumbnail|large)/$',
+                views.CanvasImage.as_view(), name='canvas-image'),
 
-    url(r'^library/(?P<slug>[-\w]+)/gallery/(?P<short_id>[a-z0-9]+)/$',
-        views.CanvasDetail.as_view(), name='canvas-detail'),
-    # canvas image views
-    url(r'^library/(?P<slug>[-\w]+)/gallery/images/(?P<page_num>[0-9]{1,4})-?[0-9]*[a-z]?/$',
-        views.CanvasImageByPageNumber.as_view(), {'mode': 'by-page'}, name='canvas-by-page'),
-    url(r'^library/(?P<slug>[-\w]+)/gallery/images/(?P<short_id>[a-z0-9]+)/(?P<mode>thumbnail|large)/$',
-        views.CanvasImage.as_view(), name='canvas-image'),
-
-    # default thumbnail for a book
-    url(r'^library/(?P<slug>[-\w]+)/images/thumbnail/$',
-        views.CanvasImage.as_view(), {'mode': 'thumbnail'}, name='book-thumbnail')
-
+            # default thumbnail for a book
+            url(r'^thumbnail/$',
+                views.CanvasImage.as_view(), {'mode': 'thumbnail'}, name='book-thumbnail')
+        ])) # end library/<slug> urls
+    ])) # end library urls
 ]
