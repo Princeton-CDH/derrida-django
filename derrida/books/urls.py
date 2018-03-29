8abd88c7dfbfde51baf1d666ff4370b24e5bfc4f
rlskoeser <rebecca.s.koeser@princeton.edu>
2018-03-29 16:45:23 -0400

    Allow roman numerals in canvas by page url

--- derrida/books/urls.py
+++ derrida/books/urls.py
@@ -40,7 +40,7 @@ urlpatterns = [
                 views.CanvasDetail.as_view(), name='canvas-detail'),
             url(r'^suppress-images/$', views.CanvasSuppress.as_view(), name='suppress-canvas'),
             # canvas image views
-            url(r'^gallery/images/(?P<page_num>[0-9]{1,4})-?[0-9]*[a-z]?(?P<x>@2x)?/$',
+            url(r'^gallery/images/(?P<page_num>[0-9vix]{1,4})-?[0-9]*[a-z]?(?P<x>@2x)?/$',
                 views.CanvasImageByPageNumber.as_view(), {'mode': 'by-page'}, name='canvas-by-page'),
             url(r'^gallery/images/(?P<short_id>[a-z0-9-]+)/(?P<mode>smthumb|thumbnail|large|info)(?P<x>@2x)?/$',
                 views.CanvasImage.as_view(), name='canvas-image'),
