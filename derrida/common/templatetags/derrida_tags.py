5b83f82f37a5ac707334cc218659655445156a04
rlskoeser <rebecca.s.koeser@princeton.edu>
2018-05-01 14:39:14 -0400

    Tweak pages and styles for "how to cite" content

--- derrida/common/templatetags/derrida_tags.py
+++ derrida/common/templatetags/derrida_tags.py
@@ -1,6 +1,10 @@
 from django import template
+from django.utils.timezone import now
+from django.template.defaultfilters import stringfilter
+
 
 register = template.Library()
+
 
 @register.simple_tag(takes_context=True)
 def querystring_replace(context, **kwargs):
@@ -23,42 +27,10 @@ def querystring_replace(context, **kwargs):
     return querystring.urlencode()
 
 
-@register.filter()
-def format_for_home(context):
-    '''Template filter to format richtext from content management
-    to the html needed to render the layout of the homepage.
-    The main content is to be contained between blocks
-
-        [main] ... [/main]
-
-    The credits content is contained with blocks much like the main block
-    with a few additonal expectations - the credit role is expected to be
-    wrapped in a strong tag and the name of the person wrapped in an em tag.
-
-        [credits]
-            <strong>Title</strong>
-            <em>Name</em>
-            <em>Name</em>
-            ...
-        [/credits]
-
-    Example use:
-
-        {{ page.richtextpage.content|richtext_filters|format_for_home|safe }}
-    '''
-
-    def getContentForBlock(tag):
-        start = "<p>[%s]</p>" % tag
-        end = "<p>[/%s]</p>" % tag
-        return context[context.index(start):context.index(end)][len(start):]
-
-    mainContent = "<section>%s</section>" % getContentForBlock('main')
-    creditContent = "<aside><dl class='credits'>%s</dl></aside>" % \
-                    getContentForBlock('credits')\
-                    .replace('<strong>', '<dt class="credits__role">')\
-                    .replace('</strong>', '</dt>')\
-                    .replace('<em>', '<dd class="credits__name">')\
-                    .replace('</em>', '</dd>')\
-                    .replace('<br>', '')
-
-    return mainContent + creditContent
+@register.filter(is_safe=True)
+@stringfilter
+def format_citation(text, sw_version):
+    '''Simple string filter for "how to cite" page to automatically
+    add current software version from the context and current date.'''
+    return text.replace('[SW_VERSION]', sw_version) \
+        .replace('[DATE]', now().strftime('%d %B %Y'))
