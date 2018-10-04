248bbda9b451f37ff8c1ef1beb5aa1adf7259ddb
Rebecca Sutton Koeser <rlskoeser@users.noreply.github.com>
2018-10-04 11:04:42 -0400

    Custom manage command to export cited items to a Zotero library (#234)
    * basic as_zotero_item method & export_zotero command (#230)
    
    * add multi creators & tests for as_zotero_item() (#230)
    
    * working export_zotero command (#230)
    
    * wip tests for command (#230)
    
    * Document new local settings needed for Zotero export script
    
    * Refactor zotero item method and test to split out type-specific logic
    
    * Refine zotero item logic and include more metadata
    
    * Refine export zotero script: clean up, report failure message, etc
    
    * Update zotero export for change in output & verbosity
    
    * Refine tests for zotero export manage command
    
    * Use item URI as Zotero url
    
    * Add project-specific notes to zotero abstract note field
    
    * Test text content in zotero abstract note
    
    * Test zotero export create_items method
    
    * Test export zotero create_items failure handling
    
    * Remove stray print statement; add zotero tag for digital edition

--- derrida/books/tests/test_commands.py
+++ derrida/books/tests/test_commands.py
@@ -1,18 +1,22 @@
 import codecs
+from collections import defaultdict
 import csv
 from io import StringIO
 import json
-from unittest.mock import patch
 import os.path
 import tempfile
+from unittest.mock import Mock, MagicMock, patch
 
 from django.core.management import call_command
-from django.test import TestCase
+from django.core.management.base import CommandError
+from django.db.models import QuerySet
+from django.test import TestCase, override_settings
 from djiffy.models import Manifest
+from pytest import raises
 
 from derrida.books.models import Instance, Reference, DerridaWork
 from derrida.books.management.commands import import_digitaleds, \
-    reference_data
+    reference_data, export_zotero
 
 
 class TestManifestImporter(TestCase):
@@ -106,6 +110,159 @@ class TestImportDigitalEds(TestCase):
         self.cmd.summarize({'urls': 3, 'manifests': 2, 'nomatch': 1})
         output = self.cmd.stdout.getvalue()
         assert 'Manifests not matched to library work instances: 1' in output
+
+@patch('pyzotero.zotero.Zotero')
+@override_settings(ZOTERO_API_KEY='z_apikey', ZOTERO_LIBRARY_ID='z_group')
+class TestExportZotero(TestCase):
+    fixtures = ['sample_work_data.json']
+
+    def setUp(self):
+        self.cmd = export_zotero.Command()
+        self.cmd.stdout = StringIO()
+        self.cmd.stderr = StringIO()
+
+    def test_handle(self, zotero):
+        with patch.object(self.cmd, 'create_collections') as mock_create_collections:
+            with patch.object(self.cmd, 'create_items') as mock_create_items:
+
+                mock_create_items.return_value = {
+                    'created': 0, 'updated': 0,
+                    'unchanged': 0, 'failed': 0
+                }
+
+                # API key is required
+                with override_settings(ZOTERO_API_KEY=None):
+                    with raises(CommandError) as err:
+                        self.cmd.handle()
+                    assert 'API key must be set' in str(err)
+
+                    mock_create_collections.assert_not_called()
+                    mock_create_items.assert_not_called()
+
+                # Zotero library ID is required
+                with override_settings(ZOTERO_LIBRARY_ID=None):
+                    with raises(CommandError) as err:
+                        self.cmd.handle()
+
+                    assert 'library ID must be set' in str(err)
+
+                # Should initialize a library with provided values
+                self.cmd.handle()
+                assert zotero.called_once_with(('z_apikey', 'group', 'z_group'))
+                # Should call create_collections with new works
+                dlg = DerridaWork.objects.get(pk=1) # no zotero id ("new")
+                assert self.cmd.create_collections.called_once_with(QuerySet(dlg))
+                # Should call create_items with all cited instances
+                assert self.cmd.create_items.called_once_with(Instance.objects.all())
+
+    def test_create_collections(self, zotero):
+        # Should report if nothing in queryset
+        # not in verbose mode; should not report anything
+        self.cmd.create_collections(DerridaWork.objects.none())
+        output = self.cmd.stdout.getvalue()
+        assert not output
+
+        # should report nothing to do when verbosity is higher
+        self.cmd.verbosity = 2
+        self.cmd.create_collections(DerridaWork.objects.none())
+        output = self.cmd.stdout.getvalue()
+        assert 'No collections' in output
+
+        # Should output how many new works were provided
+        self.cmd.library = Mock()
+        zotero_id = '12345'
+        zotero_id2 = '67890'
+        # zotero returns a dict
+        self.cmd.library.create_collections.return_value = {
+            'success': {'0': zotero_id, '1': zotero_id2}
+        }
+        self.cmd.create_collections(DerridaWork.objects.all())
+        output = self.cmd.stdout.getvalue()
+        assert 'Found 2 new Derrida works' in output
+
+        derridaworks = DerridaWork.objects.all()
+
+        # Should call create_collections with new work data
+        args = self.cmd.library.create_collections.call_args[0]
+        # first arg should be the list of collection names
+        for dwork in derridaworks:
+            assert {'name': dwork.short_title} in args[0]
+        # Should save returned zotero IDs in the database
+        assert derridaworks[0].zotero_id == zotero_id
+        assert derridaworks[1].zotero_id == zotero_id2
+
+    @patch.object(Instance, 'as_zotero_item')
+    def test_create_items(self, mock_as_zotero_item, zotero):
+        # use mock for zotero library instance
+        self.cmd.library = Mock()
+        self.cmd.library.count_items.side_effect = [0, 3]
+        self.cmd.library.last_modified_version.return_value = 'Tuesday'
+        zotero_response = {
+            'success': {
+                '0': 'abc',
+                '1': 'def',
+                '2': 'ghi'
+            },
+            'unchanged': {},
+            'failed': {}
+        }
+        self.cmd.library.create_items.return_value = zotero_response
+
+        # Should report if no items to report
+        # not in verbose mode; should not report anything
+        self.cmd.create_items(Instance.objects.none())
+        output = self.cmd.stdout.getvalue()
+        assert not output
+
+        # should report nothing to do when verbosity is higher
+        self.cmd.verbosity = 2
+        self.cmd.create_items(Instance.objects.none())
+        output = self.cmd.stdout.getvalue()
+        assert 'No items to create' in output
+
+        # test with three records
+        test_instances = Instance.objects.all()[:3]
+
+        # Should output how many instances were provided
+        stats = self.cmd.create_items(test_instances)
+        output = self.cmd.stdout.getvalue()
+        assert 'Exporting {} instances.'.format(test_instances.count()) in output
+        # Should convert instances to zotero items
+        # convert to zotero called once per item
+        assert mock_as_zotero_item.call_count == test_instances.count()
+        mock_as_zotero_item.assert_called_with(self.cmd.library)
+        # Should call create_items with the item data
+        self.cmd.library.create_items.assert_called_with(
+            [mock_as_zotero_item.return_value for i in range(3)],
+            last_modified='Tuesday')
+        # Should return summary statistics
+        assert stats['updated'] == 3
+        assert stats['created'] == 3
+        assert stats['unchanged'] == 0
+        assert stats['failed'] == 0
+
+        # Should save returned zotero IDs to the instances
+        for index, inst in enumerate(test_instances):
+            assert Instance.objects.get(pk=inst.pk).zotero_id == \
+                zotero_response['success'][str(index)]
+
+        # simulate failure; should display message
+        # - set library count to test created logic
+        self.cmd.library.count_items.side_effect = [3, 3]
+        self.cmd.stderr = StringIO()
+        error_message = '"place" is not a valid field for journalArticle'
+        zotero_response['failed'] = {
+            '0': {'message': error_message}
+        }
+        self.cmd.library.create_items.return_value = zotero_response
+        # suppress progressbar to
+        stats = self.cmd.create_items(test_instances)
+        output = self.cmd.stderr.getvalue()
+        assert 'Error on {}'.format(test_instances.first()) in output
+        assert error_message in output
+        assert stats['failed'] == 1
+        # no items created, based on mock library count
+        assert stats['created'] == 0
 
 
 class TestReferenceData(TestCase):
