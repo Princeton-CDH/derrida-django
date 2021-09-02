485426bc11a4b8aa0ef6f2f8442f0f6caee924c0
rlskoeser <rebecca.s.koeser@princeton.edu>
2021-09-02 10:30:39 -0400

    Clean up test workflow and add slack reporting

--- ci/testsettings.py
+++ ci/testsettings.py
@@ -35,13 +35,4 @@ HAYSTACK_CONNECTIONS = {
 # for unit tests, that swap test connection in for default
 HAYSTACK_TEST_CONNECTIONS = HAYSTACK_CONNECTIONS
 
-# try using system sass
-# HACK: Use different sass
-# Use local node-sass installed via npm
-# COMPRESS_PRECOMPILERS = (
-#     ('text/x-scss', 'node_modules/node-sass/bin/node-sass {infile} {outfile}'),
-# )
-
-# COMPRESS_AUTOPREFIXER_BINARY = 'node_modules/postcss-cli/bin/postcss'
-
 # secret key added as a travis build step
