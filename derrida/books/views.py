a63c36351dfb6c8f2012f524d202f96a6e520d7b
Rebecca Sutton Koeser <rlskoeser@users.noreply.github.com>
2021-09-15 16:03:23 -0400

    Update derrida/books/views.py
    Co-authored-by: Kevin McElwee <26414213+kmcelwee@users.noreply.github.com>

--- derrida/books/views.py
+++ derrida/books/views.py
@@ -711,7 +711,7 @@ class ProxyView(View):
                 local_response[header] = value
 
         # special case, for deep zoom json info response
-        if kwargs['mode'] == 'info' or (kwargs['mode'] == 'iiif' and kwargs['url'] in ['/', '/info.json']):
+        if kwargs['mode'] == 'info' or (kwargs['mode'] == 'iiif' and kwargs['url'] in ['/', '/info.json', '']):
             data = remote_response.json(object_pairs_hook=OrderedDict)
             # need to adjust the id to be relative to current url
             # patch in our proxy iiif interface at this url,
